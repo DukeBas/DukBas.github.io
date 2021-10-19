a0accb84083caacf6b8704a8476978b194cefed1
DukeBas <duke.bas@gmail.com>
2021-10-19 14:28:36 +0200

    First build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Bas Personal Website`,
+    description: `work in progress.`,
+    author: `DukeBas`,
+    siteUrl: `https://dukebas.github.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,8 +31,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,28 +1,29 @@
 import Seo from "../components/seo"
-import * as THREE from 'three'
-import ReactDOM from 'react-dom'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from "three"
+import ReactDOM from "react-dom"
+import React, { useRef, useState } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
 import Layout from "../components/layout"
 
-function Box(props: JSX.IntrinsicElements['mesh']) {
+function Box(props: JSX.IntrinsicElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!)
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
-  useFrame((state, delta) => { 
-    (ref.current.rotation.x += 0.01); 
-    (ref.current.rotation.y += 0.01); 
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
   })
   return (
     <mesh
       {...props}
       ref={ref}
       scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
+      onClick={event => setActive(!active)}
+      onPointerOver={event => setHover(true)}
+      onPointerOut={event => setHover(false)}
+    >
       <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={hovered ? 'green' : 'orange'} />
+      <meshStandardMaterial color={hovered ? "green" : "orange"} />
     </mesh>
   )
 }
@@ -32,13 +33,12 @@ let SecondPage = () => (
     <Seo title="box" />
     <span>Test</span>
     <Canvas>
-
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <Box position={[-3, 0, 0]} />
       <Box position={[3, 0, 0]} />
     </Canvas>
   </Layout>
-);
+)
 
-export default SecondPage;
+export default SecondPage
